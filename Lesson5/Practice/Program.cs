3ee65b8567e1f9c10a530ed12eb5538e203c8df3
Yaroslaw <parfenyyaroslav@yandex.ru>
2025-03-13 18:47:58 +0300

    Практика 5 урока А и Б сделана

--- Lesson5/Practice/Program.cs
+++ Lesson5/Practice/Program.cs
@@ -1,1 +1,31 @@
 ﻿using System;
+//ПРАКТИКА А Б
+class Person{
+    public string name;
+    public int age;
+    public void Introduce(){
+        Console.WriteLine($"Привет мое имя {name}");
+    }
+    public int ages(int age){
+        if(age < 0){
+            return;
+        }
+        else{
+            Person.age = age;
+        }
+    }
+
+}
+class Programm{
+    static void Main(string[] args){
+        Person person = new Person();
+        person.name = "Yarik";
+        person.age = 52;
+
+        System.Console.WriteLine(person.name);
+        System.Console.WriteLine(person.age);
+    }
+}
+class Employee:Person{
+    public string position;
+}
