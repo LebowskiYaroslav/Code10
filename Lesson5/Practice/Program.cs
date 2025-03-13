141b2b75d8f412838da906e11e06b60d0c2c5b8a
Yaroslaw <parfenyyaroslav@yandex.ru>
2025-03-13 19:11:36 +0300

    Доделка

--- Lesson5/Practice/Program.cs
+++ Lesson5/Practice/Program.cs
@@ -25,17 +25,20 @@ person.age = 10;
 }
 class Programm{
     static void Main(string[] args){
-        Person person = new Person();
-        person.name = "Yarik";
-        person.age = 52;
-        string[] person = new Person[2];
-        person.name = "yarik";
-        person.age = 10;
-        public void Introduce(){
-            Console.WriteLine($"Привет мое имя {name}");
-        }
-        public void Introduce(){
-            Console.WriteLine($"Привет мой возраст {age}");
+        Person person1 = new Person();
+        person1.name = "Yarik22";
+        person1.age = 523;
+
+        Person person2 = new Person();
+        person2.name = "Yarik";
+        person2.age = 52;
+
+        Person[] array = new Person[2];
+        array[0] = person1;
+        array[1] = person2;
+
+        for(int i = 0; i < 2; i++){
+            array[i].Introduce();
         }
 
         System.Console.WriteLine(person.name);
